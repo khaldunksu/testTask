dc3640447f1ebf129665ed72b92ac7832253db80
ksukhaldun <kseniya.khaldun@gmail.com>
2020-02-21 01:04:40 +0300

    fix error

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,31 +1,34 @@
-
+/* eslint-disable */
 window.onload = function () {
   document.querySelector(".tab_header").addEventListener('click', fTabs);
   function fTabs(event) {
     console.log(event)
-    if (event.target.className == 'tab')
-      var dataTab = event.target.getAttribute('data-tab');
-    var tab = document.getElementsByClassName('tab');
+    if (event.target.className == 'tab'){
+      let dataTab = event.target.getAttribute('data-tab');
+      let tab = document.getElementsByClassName('tab');
+
+
 
     for (let i = 0; i < tab.length; i++) {
       tab[i].classList.remove('active')
     }
     event.target.classList.add('active');
-    var tabBody = document.getElementsByClassName('content-tab')
+    let tabBody = document.getElementsByClassName('content-tab')
     for (let i = 0; i < tabBody.length; i++) {
       if (dataTab == i) {
         tabBody[i].style.display = 'block'
       } else {
         tabBody[i].style.display = 'none'
       }
+    }
 
     }
   }
 }
 
-var modal = document.getElementById('myModal');
-var btn = document.getElementById("popover_button");
-var span = document.getElementsByClassName("close")[0];
+let modal = document.getElementById('myModal');
+let btn = document.getElementById("popover_button");
+let span = document.getElementsByClassName("close")[0];
 btn.onclick = function () {
   modal.style.display = "block";
 }
@@ -41,11 +44,11 @@ window.onclick = function (event) {
 }
 
 function clock() {
-  var x = new Date(2020, 9, 1, 00, 00, 00)
-  var d = new Date();
-  var day = d.getDate();
-  var hours = d.getHours();
-  var minutes = d.getMinutes();
+  let x = new Date(2020, 9, 1, 00, 00, 00)
+  let d = new Date();
+  let day = d.getDate();
+  let hours = d.getHours();
+  let minutes = d.getMinutes();
 
   if (day <= 9) day = "0" + day;
   if (hours <= 9) hours = "0" + hours;
